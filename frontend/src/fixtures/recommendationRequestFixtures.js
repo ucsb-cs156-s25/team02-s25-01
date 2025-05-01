641f917bdb9fe7c43c84843e7a8df36def422eb7
Alec Sekimoto <alecsekimoto@ucsb.edu>
2025-05-01 13:24:10 -0700

    as - add javascript fixtures for RecommendationRequest table

---
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -0,0 +1,44 @@
+const recommendationRequestFixtures = {
+    oneRecommendationRequest: {
+        "id": 3,
+        "requesterEmail": "studentName@ucsb.edu",
+        "professorEmail": "professorName@ucsb.edu",
+        "explanation": "I need a letter of recommendation...",
+        "dateRequested": "2025-01-01T00:00:00",
+        "dateNeeded": "2025-05-01T00:00:00",
+        "done": true
+
+    },
+    threeRecommendationRequest: [
+        {
+            "id": 3,
+            "requesterEmail": "studentName@ucsb.edu",
+            "professorEmail": "professorName@ucsb.edu",
+            "explanation": "I need a letter of recommendation...",
+            "dateRequested": "2025-01-01T00:00:00",
+            "dateNeeded": "2025-05-01T00:00:00",
+            "done": true
+        },
+        {
+            "id": 4,
+            "requesterEmail": "studentName1@ucsb.edu",
+            "professorEmail": "professorName1@ucsb.edu",
+            "explanation": "I need a recommendation letter...",
+            "dateRequested": "2025-01-02T00:00:00",
+            "dateNeeded": "2025-05-02T00:00:00",
+            "done": false
+        },
+        {
+            "id": 5,
+            "requesterEmail": "studentName2@ucsb.edu",
+            "professorEmail": "professorName2@ucsb.edu",
+            "explanation": "I need a recommendation letter for...",
+            "dateRequested": "2025-01-03T00:00:00",
+            "dateNeeded": "2025-05-03T00:00:00",
+            "done": false
+        }
+
+    ]
+};
+
+export { recommendationRequestFixtures };
