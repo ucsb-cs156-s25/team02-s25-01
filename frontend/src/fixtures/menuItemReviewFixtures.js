a4594b91b1e3a98b9927b0873b32c6e342baf3ef
Oscar Luna <oscar_luna@uscb.edu>
2025-05-01 18:46:19 -0700

    fix frontend code formatting

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -5,7 +5,7 @@ const menuItemReviewFixtures = {
     reviewerEmail: "chris_gaucho@ucsb.edu",
     stars: 5,
     dateReviewed: "2025-01-20T14:20:00",
-    comments: "The Pizza was great!"
+    comments: "The Pizza was great!",
   },
   threeMenuItemReviews: [
     {
@@ -14,7 +14,7 @@ const menuItemReviewFixtures = {
       reviewerEmail: "chris_gaucho@ucsb.edu",
       stars: 5,
       dateReviewed: "2025-01-20T14:20:00",
-      comments: "The Pizza was great!"
+      comments: "The Pizza was great!",
     },
     {
       id: 3,
@@ -22,7 +22,7 @@ const menuItemReviewFixtures = {
       reviewerEmail: "bob_gaucho@ucsb.edu",
       stars: 4,
       dateReviewed: "2025-03-12T13:20:00",
-      comments: "The burger was great!"
+      comments: "The burger was great!",
     },
     {
       id: 4,
@@ -30,7 +30,7 @@ const menuItemReviewFixtures = {
       reviewerEmail: "becky_gaucho@ucsb.edu",
       stars: 1,
       dateReviewed: "2025-04-01T10:20:00",
-      comments: "The soup was horrible!"
+      comments: "The soup was horrible!",
     },
   ],
 };
