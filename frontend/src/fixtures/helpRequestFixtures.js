33bdcdc76e071e005e8b4367f0eae4ced71506ec
Hao Jin <hjin133@ucsb.edu>
2025-05-01 18:03:37 -0700

    hj - create fixtures for helprequest table

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,43 +1,42 @@
 const helpRequesrFixtures = {
-    oneHelpRequest: {
-        "id": 2,
-        "requesterEmail": "hjin133@ucsb.edu",
-        "teamId": "01",
-        "tableOrBreakoutRoom": "table1",
-        "requestTime": "2025-05-01T17:22:00",
-        "explanation": "I got an error message",
-        "solved": true
+  oneHelpRequest: {
+    id: 2,
+    requesterEmail: "hjin133@ucsb.edu",
+    teamId: "01",
+    tableOrBreakoutRoom: "table1",
+    requestTime: "2025-05-01T17:22:00",
+    explanation: "I got an error message",
+    solved: true,
+  },
+  threeHelpRequests: [
+    {
+      id: 2,
+      requesterEmail: "hjin133@ucsb.edu",
+      teamId: "01",
+      tableOrBreakoutRoom: "table1",
+      requestTime: "2025-05-01T17:22:00",
+      explanation: "I got an error message",
+      solved: true,
     },
-    threeHelpRequests: [
-        {
-          "id": 2,
-          "requesterEmail": "hjin133@ucsb.edu",
-          "teamId": "01",
-          "tableOrBreakoutRoom": "table1",
-          "requestTime": "2025-05-01T17:22:00",
-          "explanation": "I got an error message",
-          "solved": true
-        },
-        {
-          "id": 3,
-          "requesterEmail": "anyone@ucsb.edu",
-          "teamId": "11",
-          "tableOrBreakoutRoom": "BreakoutRoom 3",
-          "requestTime": "2025-04-28T18:00:00",
-          "explanation": "I need a help",
-          "solved": false
-        },
-        {
-          "id": 4,
-          "requesterEmail": "somebody@ucsb.edu",
-          "teamId": "02",
-          "tableOrBreakoutRoom": "BreakoutRoom 1",
-          "requestTime": "2025-04-30T18:00:00",
-          "explanation": "I need technical assistance",
-          "solved": true
-        }
+    {
+      id: 3,
+      requesterEmail: "anyone@ucsb.edu",
+      teamId: "11",
+      tableOrBreakoutRoom: "BreakoutRoom 3",
+      requestTime: "2025-04-28T18:00:00",
+      explanation: "I need a help",
+      solved: false,
+    },
+    {
+      id: 4,
+      requesterEmail: "somebody@ucsb.edu",
+      teamId: "02",
+      tableOrBreakoutRoom: "BreakoutRoom 1",
+      requestTime: "2025-04-30T18:00:00",
+      explanation: "I need technical assistance",
+      solved: true,
+    },
+  ],
+};
 
-    ],
-  };
-  
-  export { helpRequesrFixtures };
+export { helpRequesrFixtures };
