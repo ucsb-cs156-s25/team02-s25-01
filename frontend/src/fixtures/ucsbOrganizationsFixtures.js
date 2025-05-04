803e94770cc7e91bdb56247621fe3d3fc86dab2b
Huang Lexuan <huanglexuan@MacBook-Air.local>
2025-05-03 21:18:19 -0700

    create fixtures of ucsborganization table

---
+++ frontend/src/fixtures/ucsbOrganizationsFixtures.js
@@ -0,0 +1,33 @@
+const ucsbOrganizationFixtures = {
+  oneOrganization: [
+    {
+      orgCode: "ZPR",
+      orgTranslationShort: "Zeta Phi Rho",
+      orgTranslation: "Zeta Phi Rho",
+      inactive: false,
+    },
+  ],
+
+  threeOrganization: [
+    {
+      orgCode: "ZPR",
+      orgTranslationShort: "Zeta Phi Rho",
+      orgTranslation: "Zeta Phi Rho",
+      inactive: false,
+    },
+    {
+      orgCode: "SKI",
+      orgTranslationShort: "Skiing Club",
+      orgTranslation: "Skiing Club at UCSB",
+      inactive: false,
+    },
+    {
+      orgCode: "CES",
+      orgTranslationShort: "Student Career Center"
+      orgTranslation: "Office of Student Career Center",
+      inactive: false,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
