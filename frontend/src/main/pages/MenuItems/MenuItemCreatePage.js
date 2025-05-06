450c989623ed9f2e210a43caefff0678ec4aa548
eyob-tesh <92474515+EyobTe@users.noreply.github.com>
2025-05-06 15:48:36 -0700

    eyobte - replace menu items create placeholder with real create page logic

--- frontend/src/main/pages/MenuItems/MenuItemCreatePage.js
+++ frontend/src/main/pages/MenuItems/MenuItemCreatePage.js
@@ -1,11 +1,48 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemForm from "main/components/MenuItems/MenuItemForm";
+import { Navigate } from "react-router-dom";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function MenuItemsCreatePage() {
-  // Stryker disable all : placeholder for future implementation
+export default function MenuItemCreatePage({ storybook = false }) {
+  const objectToAxiosParams = (menuItem) => ({
+    url: "/api/UCSBDiningCommonsMenuItem/post",
+    method: "POST",
+    params: {
+      diningCommonsCode: menuItem.diningCommonsCode,
+      name: menuItem.name,
+      station: menuItem.station,
+    },
+  });
+
+  const onSuccess = (menuItem) => {
+    toast(
+      `New menu Item Created - id: ${menuItem.id} name: ${menuItem.name}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/UCSBDiningCommonsMenuItem/all"], // mutation makes this key stale so that pages relying on it reload
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/menuItems" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Menu Item</h1>
+        <MenuItemForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   );
