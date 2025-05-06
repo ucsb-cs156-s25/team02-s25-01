e02f2e887273083720cadca4c5f776473221fe08
eyob-tesh <92474515+EyobTe@users.noreply.github.com>
2025-05-06 16:24:05 -0700

    eyobte - add storybook entry for MenuItemCreatePage also had a small format edit for MenuItemEditPage

--- frontend/src/main/pages/MenuItems/MenuItemEditPage.js
+++ frontend/src/main/pages/MenuItems/MenuItemEditPage.js
@@ -1,6 +1,6 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function MenuItemsEditPage() {
+export default function PlaceholderEditPage() {
   // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
