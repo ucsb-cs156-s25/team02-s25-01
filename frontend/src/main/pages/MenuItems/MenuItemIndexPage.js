5f38ea466e028e7a6ef9f43791eb2ad2c68fcf0d
eyob-tesh <92474515+EyobTe@users.noreply.github.com>
2025-05-07 14:49:31 -0700

    eyobte - add menu item index page with stories and test

--- frontend/src/main/pages/MenuItems/MenuItemIndexPage.js
+++ frontend/src/main/pages/MenuItems/MenuItemIndexPage.js
@@ -1,17 +1,46 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
 
-export default function MenuItemsIndexPage() {
-  // Stryker disable all : placeholder for future implementation
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemTable from "main/components/MenuItems/MenuItemTable";
+import { useCurrentUser, hasRole } from "main/utils/currentUser";
+import { Button } from "react-bootstrap";
+
+export default function MenuItemIndexPage() {
+  const currentUser = useCurrentUser();
+
+  const {
+    data: menuItems,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/UCSBDiningCommonsMenuItem/all"],
+    { method: "GET", url: "/api/UCSBDiningCommonsMenuItem/all" },
+    // Stryker disable next-line all : don't test default value of empty list
+    [],
+  );
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+      return (
+        <Button
+          variant="primary"
+          href="/menuItem/create"
+          style={{ float: "right" }}
+        >
+          Create Menu Item
+        </Button>
+      );
+    }
+  };
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/menuItems/create">Create</a>
-        </p>
-        <p>
-          <a href="/menuItems/edit/1">Edit</a>
-        </p>
+        {createButton()}
+        <h1>Menu Item</h1>
+        <MenuItemTable menuItems={menuItems} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );
