45aecb5ea48d7e773df343f15f3f464db95436bc
Hao Jin <hjin133@ucsb.edu>
2025-05-09 15:44:55 -0700

    hj fix code

--- src/test/java/edu/ucsb/cs156/example/web/HelpRequestWebIT.java
+++ src/test/java/edu/ucsb/cs156/example/web/HelpRequestWebIT.java
@@ -1,5 +1,4 @@
 package edu.ucsb.cs156.example.web;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +7,11 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-
 import java.time.LocalDateTime;
-
 import edu.ucsb.cs156.example.WebTestCase;
 import edu.ucsb.cs156.example.entities.HelpRequest;
 import edu.ucsb.cs156.example.repositories.HelpRequestRepository;
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("integration")
@@ -24,52 +19,49 @@ import edu.ucsb.cs156.example.repositories.HelpRequestRepository;
 public class HelpRequestWebIT extends WebTestCase {
 
     @Autowired
-        HelpRequestRepository helpRequestRepository;
+    HelpRequestRepository helpRequestRepository;
 
     @Test
-    public void admin_user_can_create_edit_delete_helpRequest() throws Exception {
-
+    public void admin_user_can_create_edit_delete_helprequest() throws Exception {
         LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
-
-        String email1 = "hjin133@ucsb.edu";
-
         HelpRequest helprequest = HelpRequest.builder()
-            .requesterEmail(email1)
+            .requesterEmail("hjin133@ucsb.edu")
             .teamId("01")
             .tableOrBreakoutRoom("table1")
             .requestTime(ldt1)
             .explanation("Can-i-get-a-help?")
             .solved(true)
             .build();
-                                
         helpRequestRepository.save(helprequest);
-
         setupUser(true);
 
         page.getByText("Help Request").click();
 
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail")).hasText(email1);
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail"))
+                .hasText("hjin133@ucsb.edu");
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail")).hasText("hjin133@ucsb.edu");
 
         page.getByTestId("HelpRequestTable-cell-row-0-col-Delete-button").click();
 
-        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-requesterEmail")).not().isVisible();
+        assertThat(page.getByTestId("HelRequestTable-cell-row-0-col-name")).not().isVisible();
+        assertThat(page.getByTestId("HelRequestTable-cell-row-0-col-requesterEmail")).not().isVisible();
     }
+
     @Test
     public void regular_user_cannot_create_helpRequest() throws Exception {
         setupUser(false);
-    
         page.getByText("Help Request").click();
-    
+
         assertThat(page.getByText("Create HelpRequest")).not().isVisible();
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-name")).not().isVisible();
     }
 
     @Test
-    public void admin_user_can_see_create_helpRequest_button() throws Exception {
+    public void admin_user_can_see_create_helpRequest_botton() throws Exception {
         setupUser(true);
-    
+
         page.getByText("Help Request").click();
-    
+
         assertThat(page.getByText("Create HelpRequest")).isVisible();
     }
-
 }
