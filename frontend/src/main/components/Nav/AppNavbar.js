c3eb25d53750fbbed59bd5f6f97526a05643e99d
ranchen <ranchen@ucsb.edu>
2025-05-11 16:40:32 -0700

    add articles indexing page and navigation bar

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -81,6 +81,9 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/menuItems">
                     Menu Items
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/articles">
+                    Articles
+                  </Nav.Link>
                 </>
               ) : (
                 <></>
